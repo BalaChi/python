899eceb049a285165f35c3add27b357873e8b00a
BalaChi <balachandhar2003jp@gmail.com>
2023-02-09 19:07:55 +0530

    uploded modules

--- modules/main.py
+++ modules/main.py
@@ -1,15 +1,15 @@
 # from utility import add,div # or we can import all by *
-# from shopping.more_shopping import shopping_cart 
-# print(__name__,"xx")
-# print(shopping_cart.buy("apple"))
-# print(shopping_cart.max())
-# print(max(1,2))
+from shopping.more_shopping import shopping_cart 
+print(__name__,"xx")
+print(shopping_cart.buy("apple"))
+print(shopping_cart.max())
+print(max(1,2))
 
-# # every package should contain an __init__.py file
+# every package should contain an __init__.py file
 
 
-# if __name__ == '__main__':
-#     print("heyyy")
+if __name__ == '__main__':
+    print("heyyy")
 
 import sys
 sys.argv
