04fae72e7b911c5461e65a748b9f6d79f8c79622
BalaChi <balachandhar2003jp@gmail.com>
2023-02-10 19:30:08 +0530

    added debugging

---
+++ debugging/debug.py
@@ -0,0 +1,17 @@
+# debugging
+
+# linting -- extension for highlitining
+
+# ide/editor
+
+# read error 
+
+# print input 
+
+# use pdb module
+
+import pdb
+def func(n1,n2):
+    pdb.set_trace()
+    return n1+n2
+func(4,"fwf")
